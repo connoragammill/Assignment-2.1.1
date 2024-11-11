b37f52669001082534b2b2dd4f2033b1c80ef24b
conno <conno@CONSTRAPCUM>
2024-11-11 11:43:38 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Assignment_2._1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.StudentId = 202003;
+            student.FirstName = "Connor";
+            student.LastName = "Gammill";
+            student.StudentGrade = "C+";
+            
+            Console.WriteLine("Student ID:" + student.StudentId);
+            Console.WriteLine("First Name:" + student.FirstName);
+            Console.WriteLine("Last Name:" + student.LastName);
+            Console.WriteLine("Student Grade:" + student.StudentGrade);
+        }
+    }
+
+    public class Student
+    {
+        private int studentId;
+        private string firstName;
+        private string lastName;
+        private string studentGrade;
+
+        public int StudentId
+        {
+            get { return studentId; }
+            set { studentId = value; }
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string StudentGrade
+        {
+            get { return studentGrade; }
+            set { studentGrade = value; }
+        }
+    }
+
+    
+}
